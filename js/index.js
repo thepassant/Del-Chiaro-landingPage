d262334e13acc064114723061394b25c8d02be38
thepassant <bassant.ayman69@gmail.com>
2022-09-11 12:09:00 +0300

    add go to top functionality

--- js/index.js
+++ js/index.js
@@ -1,10 +1,13 @@
 const onLoad = () => {
+  //go to top functionality
+  $("#go-to-top").click(function () {
+    $("html, body").animate({ scrollTop: "0px" }, 300);
+    return false;
+  });
+
   //set copy write year
-  const setCopyWriteYear = () => {
-    const span = document.getElementById("year");
-    span.innerText = new Date().getFullYear().toString();
-  };
-  setCopyWriteYear();
+  const span = document.getElementById("year");
+  span.innerText = new Date().getFullYear().toString();
 };
 
 window.onLoad = onLoad();
