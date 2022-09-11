3624da3516fb1bd6ce00258129ce695045f21b14
thepassant <bassant.ayman69@gmail.com>
2022-09-11 21:02:20 +0300

    update hero swiper styles

--- js/index.js
+++ js/index.js
@@ -9,6 +9,38 @@ const onLoad = () => {
   const span = document.getElementById("year");
   span.innerText = new Date().getFullYear().toString();
 
+  //hero swiper
+  const heroSwiper = new Swiper(".hero-swiper-container", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 6000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      renderBullet: function (index, className) {
+        return `<span class="${className}">${index + 1}</span>`;
+      },
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  //pause autoplay on hover over title or button
+  $(".hero-swiper-container .title, .hero-swiper-container .button").hover(
+    function () {
+      heroSwiper.autoplay.stop();
+    },
+    function () {
+      heroSwiper.autoplay.start();
+    }
+  );
+
   //showcase swiper
   const showcaseSwiper = new Swiper(".show-swiper-container", {
     slidesPerView: 1,
@@ -31,6 +63,7 @@ const onLoad = () => {
     },
   });
 
+  //pause autoplay on hover over title or button
   $(".show-swiper-container .title, .show-swiper-container .button").hover(
     function () {
       showcaseSwiper.autoplay.stop();
